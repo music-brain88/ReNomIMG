8ca47710adbba9c91e46f7613293d0fdfa49f689
Yamaguchi Toshihiro <t-yamaguchi@seichogroup.com>
2018-06-22 09:30:13 +0900

    Add docstring to augmentation function

--- renom_img/api/utility/augmentation/augmentation.py
+++ renom_img/api/utility/augmentation/augmentation.py
@@ -4,11 +4,49 @@ from renom_img.api.utility.augmentation.process import MODE
 
 
 class Augmentation(object):
+    """This class is for applying augmentation to images.
+    Instance of augmentation is passed to ImageDistributor module,
+    and is called only when training process is runnning.
+    You could choose augmentation methods from Process module.
 
+    Attributes:
+        process_list (list of Process modules): list of Process modules.
+        You could choose from Flip, Shift, Rotate and WhiteNoise
+
+    Example:
+        >>> from renom_img.api.utility.augmentation.process import Flip, Shift, Rotate, WhiteNoise
+        >>> from renom_img.api.utility.augmentation.augmentation import Augmentation
+        >>> from renom_img.api.utility.distributor.distributor import ImageDistributor
+        >>> aug = Augmentation([
+        ...     Shift(40, 40),
+        ...     Rotate(),
+        ...     Flip(),
+        ...     WhiteNoise()
+        ... ])
+        >>> distributor = ImageDistributor(
+        ...     img_path_list,
+        ...     label_list,
+        ...     builder,
+        ...     aug,
+        ...     num_worker
+        ... )
+
+    """
     def __init__(self, process_list):
         self._process_list = process_list
 
     def __call__(self, x, y=None, mode="classification"):
+        """This function is for applying augmentation to images.
+
+        Args:
+            x (list of numpy.ndarray): List of images.
+            y (list of dict): List of annotation results.
+
+        Returns:
+            x (list of numpy.ndarray): List of transformed images.
+            y (list of dict): List of annotation results.
+
+        """
         return self.transform(x, y, mode)
 
     def transform(self, x, y=None, mode="classification"):
