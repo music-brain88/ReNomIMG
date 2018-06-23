564b951181f4361970f3762d13291d3ecb2b6e8f
suwa <suwa@gridsolar.jp>
2018-06-23 12:53:46 +0900

    Add fit method to yolov1

--- renom_img/api/detection/yolo_v1.py
+++ renom_img/api/detection/yolo_v1.py
@@ -1,9 +1,12 @@
 import numpy as np
 import renom as rm
+from tqdm import tqdm
 from PIL import Image
+
 from renom_img.api.model.darknet import Darknet
 from renom_img.api.utility.load import prepare_detection_data, load_img
 from renom_img.api.utility.box import transform2xy12
+from renom_img.api.utility.distributor.distributor import ImageDistributor
 
 
 def make_box(box):
@@ -303,3 +306,63 @@ class Yolov1(rm.Model):
 
         diff = (x - y)
         return rm.sum(diff * diff * mask.reshape(N, -1)) / N / 2.
+
+
+    def fit(self, train_img_path_list=None, train_annotation_list=None, train_image_distributor=None,
+            valid_img_path_list=None, valid_annotation_list=None, valid_image_distributor=None,
+            epoch=136, batch_size=64, callback_end_epoch=None):
+
+        if train_img_path_list is not None and train_annotation_list is not None:
+            train_dist = ImageDistributor(train_img_path_list, train_annotation_list)
+        else:
+            train_dist = train_image_distributor
+
+        assert train_dist is not None
+
+        if valid_img_path_list is not None and valid_annotation_list is not None:
+            valid_dist = ImageDistributor(valid_img_path_list, valid_annotation_list)
+        else:
+            valid_dist = valid_image_distributor
+
+        batch_loop = int(np.ceil(len(train_dist)/batch_size))
+        avg_train_loss_list = []
+        avg_valid_loss_list = []
+        for e in range(epoch):
+            bar = tqdm(range(batch_loop))
+            display_loss = 0
+            for i, (train_x, train_y) in train_dist.batch(batch_size, target_builder=self.build_data):
+                self.set_models(inference=False)
+                with self.train():
+                    loss = self.loss(self(train_x), train_y)
+                    reg_loss = loss + self.regularize()
+                reg_loss.grad().update(self.get_optimizer(e, epoch, i, batch_loop))
+                try:
+                    loss = loss.as_ndarray()[0]
+                except:
+                    loss = loss.as_ndarray()
+                display_loss += loss
+                bar.set_description("Epoch:{:03d} Train Loss:{:5.3f}".format(e, loss))
+                bar.update(1)
+            avg_train_loss = display_loss/(i+1)
+            avg_train_loss_list.append(avg_train_loss)
+
+            if valid_dist is not None:
+                display_loss = 0
+                for i, (valid_x, valid_y) in valid_dist.batch(batch_size, target_builder=self.build_data):
+                    self.set_models(inference=True)
+                    loss = self.loss(self(train_x), train_y)
+                    try:
+                        loss = loss.as_ndarray()[0]
+                    except:
+                        loss = loss.as_ndarray()
+                    display_loss += loss
+                    bar.set_description("Epoch:{:03d} Valid Loss:{:5.3f}".format(e, loss))
+                    bar.update(1)
+                avg_valid_loss = display_loss/(i+1)
+                avg_valid_loss_list.append(avg_train_loss)
+                bar.set_description("Epoch:{:03d} Avg Train Loss:{:5.3f} Avg Valid Loss:{:5.3f}".format(e, avg_train_loss, avg_valid_loss))
+            else:
+                bar.set_description("Epoch:{:03d} Avg Train Loss:{:5.3f}".format(e, avg_train_loss))
+            bar.close()
+            callback_end_epoch(e, self, avg_train_loss_list, avg_valid_loss_list)
+        return avg_train_loss_list, avg_valid_loss_list
