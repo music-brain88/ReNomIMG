2e97ec5c13d86e74dd8888cabe2229e93e0eeee4
junyamada <junyamada107@gmail.com>
2018-06-25 14:31:02 +0900

    add test

--- test/python/test_utils.py
+++ test/python/test_utils.py
@@ -1,36 +1,27 @@
+import os, sys
+import shutil
 import pytest
-from renom_img.api.utils.nms import nms
-from renom_img.api.utils.load import parse_xml_detection
-from renom_img.api.utils.target import build_target_yolo, build_target
 
+import numpy as np
+from renom_img.api.utility.augmentation.process import contrast_norm
+from PIL import Image
 
-@pytest.mark.parametrize("box_list, answer, threashold, return_type", [
-    [[(10, 10, 20, 20), (5, 5, 20, 20)], [(5, 5, 20, 20)], 0.2, 'box'],
-    [[(10, 10, 20, 20), (5, 5, 20, 20)], [1], 0.2, 'index']
+@pytest.fixture(scope='session', autouse=True)
+def scope_session():
+    if os.path.exists('outputs'):
+        shutil.rmtree('outputs')
+    os.mkdir('outputs')
 
-])
-def test_nms(box_list, answer, threashold, return_type):
-    result = nms(box_list, threashold, return_type)
-    assert result == answer
+def test_contrast_norm():
+    img = Image.open('./renom.png')
+    img.convert('RGB')
+    img = np.array(img).transpose(2, 0, 1).astype(np.float)
+    x = np.array([img])
+    y = [1]
+    alpha = 0.5
+    answer = alpha * (x-128) + 128
+    result = contrast_norm(x, y, alpha=alpha)
+    Image.fromarray(np.uint8(result[0][0].transpose(1, 2, 0))).save('./outputs/test_contrast_norm.png')
+    assert (answer == result[0]).any()
+    assert y == result[1]
 
-
-def test_parse_xml_detection():
-    path = "voc.xml"
-    annotation_list = parse_xml_detection([path])
-    print("Parsed", annotation_list)
-    assert annotation_list[0][0]['name'] == 'car'
-    assert annotation_list[0][0]['box'] == [43.5, 307.0, 61.0, 58.0]
-
-
-def test_build_target_yolo():
-    path = "voc.xml"
-    annotation_list = parse_xml_detection([path])
-    target, mapping = build_target_yolo(annotation_list, cells=4, img_size=(500, 500))
-    print(target, mapping)
-
-
-def test_build_target():
-    path = "voc.xml"
-    annotation_list = parse_xml_detection([path])
-    target, mapping = build_target(annotation_list, img_size=(500, 500))
-    print(target, mapping)
