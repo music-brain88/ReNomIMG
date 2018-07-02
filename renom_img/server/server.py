45172334f2ae0e3eac5fbc4735e74e879d66d8e8
Yamaguchi Toshihiro <t-yamaguchi@seichogroup.com>
2018-07-02 11:23:47 +0900

    Fix bug

--- renom_img/server/server.py
+++ renom_img/server/server.py
@@ -24,6 +24,9 @@ from bottle import HTTPResponse, default_app, route, static_file, request, error
 
 from renom.cuda import release_mem_pool
 
+from renom_img.server import create_dirs
+create_dirs()
+
 from renom_img.api.utility.load import parse_xml_detection
 from renom_img.server import wsgi_server
 from renom_img.server.train_thread import TrainThread
@@ -31,13 +34,11 @@ from renom_img.server.prediction_thread import PredictionThread
 from renom_img.server.utility.storage import storage
 
 # Constants
-from renom_img.server import create_dirs
 from renom_img.server import MAX_THREAD_NUM, DB_DIR_TRAINED_WEIGHT
 from renom_img.server import DATASRC_IMG, DATASRC_LABEL, DATASRC_DIR, DATASRC_PREDICTION_OUT
 from renom_img.server import STATE_FINISHED, STATE_RUNNING, STATE_DELETED, STATE_RESERVED
 from renom_img.server import WEIGHT_EXISTS, WEIGHT_CHECKING, WEIGHT_DOWNLOADING
 
-create_dirs()
 executor = Executor(max_workers=MAX_THREAD_NUM)
 
 # Thread(Future object) is stored to thread_pool as pair of "thread_id:[future, thread_obj]".
