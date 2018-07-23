9e0139826fe8bb1b127437b72926aa32e903ab43
Yamaguchi Toshihiro <t-yamaguchi@seichogroup.com>
2018-07-23 19:32:28 +0900

    Fix micro average

--- renom_img/api/utility/evaluate/classification.pyx
+++ renom_img/api/utility/evaluate/classification.pyx
@@ -41,7 +41,7 @@ cpdef precision_recall_score(y_pred, y_true):
         if p == t:
             tp[p] += 1
 
-    class_ids = np.unique(np.concatenate([pred_sum.keys(), true_sum.keys()]))
+    class_ids = set(pred_sum.keys()) | set(true_sum.keys())
     precision = {}
     recall = {}
     for c in class_ids:
@@ -53,8 +53,8 @@ cpdef precision_recall_score(y_pred, y_true):
             recall[c] = float(tp[c]) / true_sum[c]
         else:
             recall[c] = None
-    mean_precision = np.sum(tp) / np.sum(pred_sum)
-    mean_recall = np.sum(tp) / np.sum(true_sum)
+    mean_precision = np.fromiter(tp.values(), dtype=float).sum() / np.fromiter(pred_sum.values(), dtype=float).sum()
+    mean_recall = np.fromiter(tp.values(), dtype=float).sum() / np.fromiter(true_sum.values(), dtype=float()).sum()
     return precision, mean_precision, recall, mean_recall
 
 cpdef accuracy_score(y_pred, y_true):
