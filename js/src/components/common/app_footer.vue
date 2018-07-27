5329a7566b67ca9dcc42956d76eccc522702974b
Nishizaka <nishizaka.shigenari@gmail.com>
2018-07-27 16:12:17 +0900

    make footer

---
+++ js/src/components/common/app_footer.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+<footer>
+  <div class="row">
+    <div class="col-md-3">
+      <div class="media-wrapper">
+        <img :src="url">
+      </div>
+    </div>
+    <div class="9">
+
+    </div>
+  </div>
+</footer>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      url: require('../../../static/img/renomlogo.jpg')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+footer{
+  background: #1e264b;
+}
+</style>
