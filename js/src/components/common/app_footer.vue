1264fc3c375f119f74468e5a9af92747dce69175
Nishizaka <nishizaka.shigenari@gmail.com>
2018-07-30 13:35:22 +0900

    modify footer.vue

--- js/src/components/common/app_footer.vue
+++ js/src/components/common/app_footer.vue
@@ -1,24 +1,30 @@
 <template lang="html">
-<div class="row wrap">
-  <div class="col-md-12 footer-content">
-    <div class="row">
-      <div class="col-md-3">
-        <div class="media-wrapper">
-          <img :src="url">
-          <div class="content-margin-top">
-              <p>ReNom_IMG {{version}}</p>
+<footer>
+  <div class="row">
+    <div class="col-md-12 footer-content">
+      <div class="row">
+        <div class="col-md-3">
+          <div class="media-wrapper">
+            <img :src="url">
+            <div class="content-margin-top">
+                <p>ReNom_IMG {{version}}</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="9">
-
+        <div class="col-md-9">
+          <!--none -->
+        </div>
       </div>
     </div>
   </div>
-  <div class="col-md-12 copyright align-middle">
-    <p><small>&copy; 2018 GRID INC, ALL rights reserved.</small></p>
+  <div class="row wrap">
+    <div class="col-md-12 copyright align-middle">
+      <div class="copyright-text">
+        <small>&copy; 2018 GRID INC, ALL rights reserved.</small>
+      </div>
+    </div>
   </div>
-</div>
+</footer>
 </template>
 
 <script>
@@ -34,26 +40,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.wrap{
-  color: $font-color;
-  width: 100%;
-  height:200px;
+footer{
+  background: $footer-color;
+  padding: 0;
+  .row {
+    margin-left: 0px;
+    margin-right: 0px;
+  }
   .footer-content{
     padding-left: 3%;
-    margin-top: 15%;
     background: $footer-color;
   }
   .copyright{
-    padding-left: 3%;
     background: $footer-copyright-color;
-    bottom: 0;
+    .copyright-text{
+      padding-left: 3%;
+    }
   }
 }
 .content-margin-top{
   margin-top: $content-margin;
 }
 .media-wrapper{
-  margin-top: 10%;
+  margin-top: $content-margin;
 }
 
 </style>
