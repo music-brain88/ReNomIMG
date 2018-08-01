2b3abb3b8a1ebe6ac976a4e2c7e32ad369d1aaa0
Nishizaka <nishizaka.shigenari@gmail.com>
2018-08-01 11:41:09 +0900

    modify select option css

--- js/src/components/page/detection_page/model_list.vue
+++ js/src/components/page/detection_page/model_list.vue
@@ -7,14 +7,15 @@
       <div class="title">
         Model List
       </div>
-
-      <select class="sort-selectbox" v-model="selected" @change="sortModels">
-        <option disabled value="">sort by</option>
-        <option value="0">Model ID</option>
-        <option value="1">IoU</option>
-        <option value="2">mAP</option>
-        <option value="3">Validation Loss</option>
-      </select>
+      <div class="select-wrapper">
+        <select class="sort-selectbox" v-model="selected" @change="sortModels">
+          <option class="select-values" disabled value="">sort by</option>
+          <option class="select-values" value="0">Model ID</option>
+          <option class="select-values" value="1">IoU</option>
+          <option class="select-values" value="2">mAP</option>
+          <option class="select-values" value="3">Validation Loss</option>
+        </select>
+      </div>
     </div>
 
     <div class="model-item-area">
@@ -79,6 +80,32 @@ export default {
     margin-left: $content-top-heder-horizonral-margin;
   }
 
+  .select-wrapper{
+    position: relative;
+    display: inline-block;
+  }
+  .select-wrapper::before{
+    z-index: 1;
+    position: absolute;
+    right:25%;
+    top: 0;
+    font-family: "FontAwesome";
+    content: '\f107';
+    line-height: $content-top-header-hight;
+    color: $font-color;
+    pointer-events: none;
+  }
+
+  select {
+    text-align: center;
+    text-align-last: center;
+    -webkit-appearance: none;
+	  -moz-appearance: none;
+	  appearance: none;
+    background: transparent;
+    position: relative;
+    /* webkit*/
+  }
   .sort-selectbox {
     height: $selectbox-height;
     margin: 0;
@@ -91,6 +118,17 @@ export default {
     font-size: $selectbox-font-size;
     background: $header-color;
     color:$font-color;
+    // .select-values{
+    //   background: $content-bg-color;
+    //   color:#000000;
+    // }
+    // disabled .select-values{
+    //   background: #CCCCCC;
+    //   color:#000000;
+    //   :hover{
+    //     background: #BBBBBB;
+    //   }
+    // }
   }
 
   .model-item-area {
