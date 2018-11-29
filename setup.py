507ac930eecbde878370a62064d7803ebc53a329
suwa <suwa@gridsolar.jp>
2018-11-29 20:28:34 +0900

    Modify version number

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ class BuildNPM(distutils.command.build.build):
 
 setup(
     name="renom_img",
-    version="1.0.6",
+    version="1.0.7",
     entry_points=entry_points,
     packages=['renom_img'],
     install_requires=requires,
