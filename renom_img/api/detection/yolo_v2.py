1e3ad434a733f6432e8852bfbaebc1e064bc7f25
shamimtowhid <shamim.towhid@gmail.com>
2019-01-23 10:39:24 +0900

    fixed yolov2 augmentation calling

--- renom_img/api/detection/yolo_v2.py
+++ renom_img/api/detection/yolo_v2.py
@@ -441,6 +441,9 @@ class Yolov2(Detection):
             img_list, label_list = prepare_detection_data(
                 img_path_list, annotation_list, imsize_list[size_index])
 
+            if augmentation is not None:
+                img_list, label_list = augmentation(img_list, label_list, mode="detection")
+
             for n, annotation in enumerate(label_list):
                 # This returns resized image.
                 # Target processing
