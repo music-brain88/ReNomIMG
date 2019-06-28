f000e2b5a1385623efdf5a84ce3829ef1b26c4d7
takuuuuumi34 <tknm34@gmail.com>
2019-06-28 17:44:55 +0900

    add best_epoch_valid_result to polling return

--- renom_img/server/server.py
+++ renom_img/server/server.py
@@ -1870,6 +1870,7 @@ def get_train_status(task_name):
             "total_valid_batch": 0,
             "nth_valid_batch": 0,
             "best_result_changed": False,
+            "best_epoch_valid_result": saved_model["best_epoch_valid_result"],
             "train_loss_list": saved_model["train_loss_list"],
             "valid_loss_list": saved_model["valid_loss_list"],
         }
@@ -1899,6 +1900,7 @@ def get_train_status(task_name):
             "total_valid_batch": 0,
             "nth_valid_batch": 0,
             "best_result_changed": False,
+            "best_epoch_valid_result": {},
             "train_loss_list": [],
             "valid_loss_list": [],
         }
@@ -1920,6 +1922,7 @@ def get_train_status(task_name):
             "total_valid_batch": 0,
             "nth_valid_batch": 0,
             "best_result_changed": active_train_thread.best_valid_changed,
+            "best_epoch_valid_result": active_train_thread.best_epoch_valid_result,
             "train_loss_list": active_train_thread.train_loss_list,
             "valid_loss_list": active_train_thread.valid_loss_list,
         }
