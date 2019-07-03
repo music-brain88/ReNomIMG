868ca01f275ae83002174ff66a3c5be4ae1e7107
uyama hikaru <uyama.hikaru@gridsolar.jp>
2019-07-03 18:59:32 +0900

    #330

--- js/src/components/Organisms/rnc-modal-dataset/rnc-modal-dataset.vue
+++ js/src/components/Organisms/rnc-modal-dataset/rnc-modal-dataset.vue
@@ -536,7 +536,7 @@ export default {
         height: calc(100% - 5% - 3% - 1.6rem - 3% - 1.6rem - 6% - 20px - 2% - 40px - 2%);
         overflow: auto;
         #class-ratio-bars {
-          height: 12px;
+          height: 14px;
           width: 100%;
           display: flex;
           margin: 6px 0;
